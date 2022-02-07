47af877a71b92e7cb6e941fb1da32eba9dfbdcd8
Robert Witzke <robertwitzke.rw@gmail.com>
2022-02-07 14:22:14 +0100

    posting update

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.use(express.static("/webprog"));
+app.use(express.static("./"));
+
+app.get("/", (req, res) => {
+  const html = fs.readFileSync("./index.html", "utf8")
+  res.send(html);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
