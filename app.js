284be2362afa4e76a17a749411dab2fb48ea34d3
Robert Witzke <robertwitzke.rw@gmail.com>
2022-03-21 19:12:35 +0100

    final version

--- app.js
+++ app.js
@@ -16,12 +16,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("./"));
 
 app.get("/", (req, res) => {
-  if(req.cookies.username) {
+  doesUserExist(req, res, () => {
     res.redirect("/home");
-  } else {
-    res.redirect("/showlogin")
-  }
-  res.send();
+    res.send();
+  }) 
 });
 
 app.get("/showlogin", (req, res) => {
@@ -53,58 +51,68 @@ const data = {};
 const counter = {};
 
 app.post("/favorit", (req, res) => {
-  if(!(req.cookies.username in data)) {
-    data[req.cookies.username] = [];
-  }
 
-  if(!(data[req.cookies.username].includes(req.body.favorite_btn))) {
-    data[req.cookies.username].push(req.body.favorite_btn);
-  } else {
-    data[req.cookies.username].splice(data[req.cookies.username].indexOf(req.body.favorite_btn), 1);
-  }
-
-  console.log(data);
-  res.cookie("favorites", favorites);
-  res.redirect("/home");
-  res.send();
-})
+  doesUserExist(req, res, () => {
+    if(!(req.cookies.username in data)) {
+      data[req.cookies.username] = [];
+    }
+  
+    if(!(data[req.cookies.username].includes(req.body.favorite_btn))) {
+      data[req.cookies.username].push(req.body.favorite_btn);
+    } else {
+      data[req.cookies.username].splice(data[req.cookies.username].indexOf(req.body.favorite_btn), 1);
+    }
+  
+    console.log(data);
+    res.cookie("favorites", favorites);
+    res.redirect("/home");
+    res.send();
+  })
+});
 
 app.get("/data", (req, res) => {
-  res.json(data[req.cookies.username]);
+  doesUserExist(req, res, () => {
+    res.json(data[req.cookies.username]);
+  })
 });
 
 app.get("/countdata", (req, res) => {
-  isEmpty = true;
-  visible_cities = [];
-  sum = 0;
+  doesUserExist(req, res, () => {
+    isEmpty = true;
+    visible_cities = [];
+    sum = 0;
 
-  for (let i = 0; i < counter[req.cookies.username].length; i++) {
-    sum += counter[req.cookies.username][i][1];
-  }
-
-  average = sum / 4;
-
-  for (let i = 0; i < counter[req.cookies.username].length; i++) {
-    if(counter[req.cookies.username][i][1] >= average && counter[req.cookies.username][i][1] !== 0) {
-      isEmpty = false;
-      visible_cities.push(counter[req.cookies.username][i]);
+    for (let i = 0; i < counter[req.cookies.username].length; i++) {
+      sum += counter[req.cookies.username][i][1];
     }
-  }
 
-  if(!isEmpty) {
-    res.json(visible_cities);
-  } else {
-    res.json([]);
-  }
+    average = sum / 4;
 
+    for (let i = 0; i < counter[req.cookies.username].length; i++) {
+      if(counter[req.cookies.username][i][1] >= average && counter[req.cookies.username][i][1] !== 0) {
+        isEmpty = false;
+        visible_cities.push(counter[req.cookies.username][i]);
+      }
+    }
+
+    if(!isEmpty) {
+      res.json(visible_cities);
+    } else {
+      res.json([]);
+    }
+  })
 });
 
 const comments = {};
 
 app.post("/commentary", (req, res) => {
-  comments[req.cookies.username].push(req.body.comments);
-  res.redirect(req.get('referer'));
-  res.send();
+  doesUserExist(req, res, () => {
+    if(req.body.comments.length > 0) {
+      comments[req.cookies.username].push(req.body.comments);
+    }
+    res.redirect(req.get('referer'));
+    res.send();
+  })
 });
 
 app.get("/commentsdata", (req, res) => {
@@ -119,29 +127,40 @@ app.get("/commentsdata", (req, res) => {
 });
 
 app.get("/home", (req, res) => {
-  if(req.cookies.username) {
+  doesUserExist(req, res, () => {
     res.sendFile(__dirname + "/overview.html");
-  } else {
-    res.redirect("/showlogin");
-  }
+  })
 });
 
 app.post("/count", (req, res) => {
-  if(req.body.cityname === "Barcelona") {
-    counter[req.cookies.username][0][1] += 1;
-    res.redirect("barcelona_page.html");
-  } else if(req.body.cityname === "New York") {
-    counter[req.cookies.username][1][1] += 1;
-    res.redirect("newyork_page.html");
-  } else if(req.body.cityname === "Tokio") {
-    counter[req.cookies.username][2][1] += 1;
-    res.redirect("tokio_page.html");
-  } else if(req.body.cityname === "Kapstadt") {
-    counter[req.cookies.username][3][1] += 1;
-    res.redirect("kapstadt_page.html");
-  }
-  res.send();
+
+  doesUserExist(req, res, () => {
+    if(req.body.cityname === "Barcelona") {
+      counter[req.cookies.username][0][1] += 1;
+      res.redirect("barcelona_page.html");
+    } else if(req.body.cityname === "New York") {
+      counter[req.cookies.username][1][1] += 1;
+      res.redirect("newyork_page.html");
+    } else if(req.body.cityname === "Tokio") {
+      counter[req.cookies.username][2][1] += 1;
+      res.redirect("tokio_page.html");
+    } else if(req.body.cityname === "Kapstadt") {
+      counter[req.cookies.username][3][1] += 1;
+      res.redirect("kapstadt_page.html");
+    }
+    res.send();
+  })
 })
+
+function doesUserExist(req, res, callback) {
+  if(req.cookies.username && data[req.cookies.username] && comments[req.cookies.username] && counter[req.cookies.username]) {
+    callback();
+  } else {
+    res.clearCookie("username");
+    res.clearCookie("favorites"); 
+    res.redirect("/showlogin")
+  }
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
